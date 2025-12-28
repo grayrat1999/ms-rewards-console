13b57144d7d2de54925be362dfbc333387c3c2ab
grayrat <grayrat1999@163.com>
2025-12-28 10:54:39 +0800

    add scripts

--- script/Auto.ps1
+++ script/Auto.ps1
@@ -2,29 +2,28 @@ $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 . (Join-Path $ScriptDir "Common.ps1")
 
 # 获取所有包含 "Profile" 的文件夹
-$profiles = Get-ChildItem -Path $WEB_BROWSER_DATA_DIR -Directory | Where-Object { $_.Name -like "Profile*" } | ForEach-Object { $_.Name }
+$profiles = @(Get-ChildItem -Path $WEB_BROWSER_DATA_DIR -Directory | Where-Object { $_.Name -like "Profile*" } | ForEach-Object { $_.Name })
 
 # 要排除的列表
-$exclude_list = @("")
+$exclude_profiles = @("")
 
 for ($i = 0; $i -lt $profiles.Count; $i++) {
     $item = $profiles[$i]
     $proxy_server = $PROXY_SERVERS[$i % $PROXY_SERVERS.Count]
-
-    if ($exclude_list -contains $item) {
+    if ($exclude_profiles -contains $item) {
         continue
     }
     Write-Host "No.$i $(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - $item - 使用代理[$proxy_server]"
-    pkill -f "Microsoft Edge"
 
     & $WEB_BROWSER_EXE_PATH `
-    --profile-directory="$item" `
-    --proxy-server="$proxy_server" `
+    --profile-directory=$item `
+    --proxy-server=$proxy_server `
     $BING_URL `
     > /dev/null 2>&1 &
 
-    Start-Sleep -Seconds 10
+    Start-Sleep -Seconds 1200
+    pkill -f $WEB_BROWSER
 }
 
-pkill -f "Microsoft Edge"
+pkill -f $WEB_BROWSER
 Write-Host "所有任务已完成!!"
--- script/Common.ps1
+++ script/Common.ps1
@@ -9,11 +9,14 @@ $PROXY_SERVERS = @(
 # 测试网址
 $PROXY_SERVER_TEST_URL = "https://www.baidu.com"
 
+# 浏览器
+$WEB_BROWSER = "Microsoft Edge"
+
 # 浏览器数据目录
-$WEB_BROWSER_DATA_DIR = "${Home}/Library/Application Support/Microsoft Edge"
+$WEB_BROWSER_DATA_DIR = "${Home}/Library/Application Support/$WEB_BROWSER"
 
 # 浏览器可执行文件路径
-$WEB_BROWSER_EXE_PATH = "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge"
+$WEB_BROWSER_EXE_PATH = "/Applications/${WEB_BROWSER}.app/Contents/MacOS/$WEB_BROWSER"
 
 # BING地址
 $BING_URL = "https://www.bing.com"
